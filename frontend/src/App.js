e0c9d2e23b1ec2422d81fa38c3faa00977f59ffe
Nan0NJ <89221061@student.upr.si>
2025-03-11 19:14:52 +0100

    Added Initial UI/UX implementation with Reactjs

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>Initial Load for frontend.</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
